53665ff6a1f72a15354769194adb06745f98b46a
Denis Shtefan <den.shtefan@gmail.com>
2019-03-24 22:18:12 +0300

    Getting images

--- web_lab3/js/script.js
+++ web_lab3/js/script.js
@@ -12,3 +12,14 @@ $.ajax({
     	document.getElementById('text').innerHTML='<h3>' + data.quoteText + '</h3><h3><em>'+
     	data.quoteAuthor + '</em><h3>';
 	})
+$.ajax({
+      url: "https://api.unsplash.com/photos/random",
+      data: {
+      	client_id: '7cebdd25191337c65db5e323ec502b93ed17b6b08ecd2a520ba3d98f5104f9f9'
+      }
+    })
+    .done(function(data) {
+    	document.getElementById('img').innerHTML+= '<img src="' + data.urls.small + '" />';
+	})
+
+ 
