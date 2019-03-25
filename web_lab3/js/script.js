977257e8afae9c8be32da1b354c8ba2af314e350
Denis Shtefan <den.shtefan@gmail.com>
2019-03-25 11:28:38 +0300

    Drawing images on canvas

--- web_lab3/js/script.js
+++ web_lab3/js/script.js
@@ -1,25 +1,58 @@
-$.ajax({
-      url: "https://api.forismatic.com/api/1.0/",
-      jsonp: "jsonp",
-      dataType: "jsonp",
-      data: {
-        method: "getQuote",
-        lang: "ru",
-        format: "jsonp"
-      }
-    })
-    .done(function(data) {
-    	document.getElementById('text').innerHTML='<h3>' + data.quoteText + '</h3><h3><em>'+
-    	data.quoteAuthor + '</em><h3>';
-	})
-$.ajax({
-      url: "https://api.unsplash.com/photos/random",
-      data: {
-      	client_id: '7cebdd25191337c65db5e323ec502b93ed17b6b08ecd2a520ba3d98f5104f9f9'
-      }
-    })
-    .done(function(data) {
-    	document.getElementById('img').innerHTML+= '<img src="' + data.urls.small + '" />';
-	})
+$(function() {
+  var img = new Image();
+  var x = 0,
+      y = 0,
+      dx = 200,
+      dy = 200;
 
+    canv= document.getElementById('canvas'),
+    ctx = canv.getContext('2d');
+
+  canv.width = window.innerWidth;
+  canv.height = window.innerHeight;
+
+
+  $.ajax({
+        url: "https://api.forismatic.com/api/1.0/",
+        jsonp: "jsonp",
+        dataType: "jsonp",
+        data: {
+          method: "getQuote",
+          lang: "ru",
+          format: "jsonp"
+        }
+      })
+      .done(function(data) {
+      	document.getElementById('canvas').innerHTML='<h3>' + data.quoteText + '</h3><h3><em>'+
+      	data.quoteAuthor + '</em><h3>';
+  	})
+  function getImg() {
+    $.ajax({
+          url: "https://api.unsplash.com/photos/random",
+          data: {
+          	client_id: '4385b440d6c0e7e8f89d77fd80d32f2a6fb25fb70c3b3e2a7cf629bb3dd0be99'
+          }
+        })
+        .done(function(data) {
+          img.src = data.urls.small;
+    	})
+  }
+ 
+  getImg();
+  drawImg();
+  getImg();
+  drawImg();
+  
+
+  function drawImg(){
+    img.onload = function(){
+      var 
+        canv1= document.getElementById('canvas'),
+        ctx1 = canv.getContext('2d');
+
+      ctx1.drawImage(img, x, y, 200, dy);
+      x = x + 200;
+    }
+  }
+})
  
